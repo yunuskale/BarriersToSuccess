b24eab6d5b984c31e63e45b8a90e365541f2ac3e
Melisa Boz <85584360+MelisaBOZ@users.noreply.github.com>
2023-04-08 15:07:35 +0300

    m

--- BarriersToSuccess/Assets/Scripts/Movement.cs
+++ BarriersToSuccess/Assets/Scripts/Movement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
+    public Vector3 pointA;//m Ýlk olarak, karakterin hareket edeceði iki noktanýn konumunu Vector3 türünden deðiþkenlerle tanýmladým.
+    public Vector3 pointB;//m
+    private Vector3 target;// m Daha sonra, karakterin hareket etmesini istediðimiz yönü belirleyen bir hedef deðiþkeni tanýmladým.
+    [SerializeField] private int speed;//m 
     [SerializeField] private Animator anim;
     [SerializeField] private DynamicJoystick joystick;
     [SerializeField] private Transform cameraTransform;
@@ -16,6 +19,21 @@ public class Movement : MonoBehaviour
     private float m_currentH = 0;
     [SerializeField] private Vector3 rot;
 
+    void Start()
+    {
+        target = pointA;// m Start() fonksiyonu içerisinde, hedef deðiþkenini ilk olarak pointA olarak ayarladým.
+    }
+    void FixedUpdate()
+    {
+        // m Hedefe doðru hareket et
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        // m Eðer hedefe ulaþtýysan, hedefi deðiþtir
+        if (transform.position == target)
+        {
+            target = (target == pointA) ? pointB : pointA;
+        }
+    }
     void Update()
     {
 
@@ -31,7 +49,7 @@ public class Movement : MonoBehaviour
         direction.y = 0;
         direction = direction.normalized * directionLength;
 
-        if (v != 0 && h != 0 )
+        if (v != 0 && h != 0)
         {
             anim.SetBool("run", true);
             m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * interpolation);
@@ -47,6 +65,6 @@ public class Movement : MonoBehaviour
         //Vector3 clampingPos = new Vector3(Mathf.Clamp(transform.position.x, clampX.x, clampX.y), transform.position.y, Mathf.Clamp(transform.position.z, clampZ.x, clampZ.y));
         //transform.position = clampingPos;
     }
-   
-    }
+
+}
 
